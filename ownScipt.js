2573dcbed287c8cd8a4b91027da3fdbbdc917fce
Nico <NPfeiffer96@web.de>
2016-03-03 00:40:25 +0100

    Initial commit

---
+++ ownScipt.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Nico on 02.03.2016.
+ */
+function function1(){
+    var x = "Total Width: " + screen.width + "px";
+    var y = "Total Height: " + screen.height + "px";
+    document.getElementById("demo").innerHTML = x;
+    document.getElementById("demo2").innerHTML = y;
+}
